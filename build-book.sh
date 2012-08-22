1e41ac7c8e05046ef2ee964bafb3837f8caa59b4
hadley <h.wickham@gmail.com>
2012-08-22 09:28:22 -0500

    pandoc script to build complete book pdf

---
+++ build-book.sh
@@ -0,0 +1,26 @@
+pandoc -o book.pdf \
+  Introduction.md \
+  Scoping.md \
+  Functions.md \
+  First-class-functions.md \
+  Evaluation.md \
+  Computing-on-the-language.md \
+  Exceptions-debugging.md \
+  S3.md \
+  S4.md \
+  R5.md \
+  performance.md \
+  Rcpp.md \
+  Philosophy.md \
+  package-basics.md \
+  Development.md \
+  Testing.md \
+  docs-function.md \
+  docs-package.md \
+  style.md \
+  Namespaces.md \
+  git.md \
+  Release.md \
+  Vocabulary.md
+
+open book.pdf
