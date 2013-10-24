ff4249c8a30fc10ca39836995c00ee8055294b3b
hadley <h.wickham@gmail.com>
2013-10-24 09:02:30 -0500

    Fix error in caching script

--- book/build-book.r
+++ book/build-book.r
@@ -4,7 +4,7 @@ source("_plugins/rmd2html.r")
 chapters <- dir(".", pattern = "\\.rmd$")
 
 for (chapter in chapters) {
-  out <- cache_file(chapter, rmd2md)
+  out <- cache_file(chapter, rmd2md, ".md")
   file.copy(out, paste0("book/chapters/", gsub(".rmd", ".md", chapter)))
 }
 
