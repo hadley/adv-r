f0c0a1a3632d390cd147712023ad674b14315c0d
hadley <h.wickham@gmail.com>
2014-02-18 08:51:17 -0600

    Clean up book building script

--- book/build.sh
+++ book/build.sh
@@ -5,8 +5,8 @@
 #   Functionals.md
 
 pandoc --chapters -o book/adv-r.pdf --latex-engine xelatex \
-  -V papersize:oneside -V links-as-notes --toc \
-  -M monofont=Inconsolata \
+  --variable=papersize:oneside --variable=links-as-notes \
+  --toc --metadata=monofont:Inconsolata \
   book/title.md \
   book/chapters/Introduction.md \
   book/part-foundations.md \
@@ -31,17 +31,3 @@ pandoc --chapters -o book/adv-r.pdf --latex-engine xelatex \
   book/chapters/Memory.md \
   book/chapters/Rcpp.md \
   book/chapters/C-interface.md
-
-# pandoc --chapters -o packages.pdf --latex-engine xelatex \
-#   -V papersize:oneside -V links-as-notes --toc \
-#   Philosophy.md \
-#   package-basics.md \
-#   Package-development-cycle.md \
-#   Documenting-packages.md \
-#   Documenting-functions.md \
-#   Testing.md \
-#   style.md \
-#   Namespaces.md \
-#   git.md \
-#   Release.md
-
