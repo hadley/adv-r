e7e9f183cd14e938e6a9080010feb2e74dce51f8
hadley <h.wickham@gmail.com>
2014-02-18 08:53:20 -0600

    Number sections in book

--- book/build.sh
+++ book/build.sh
@@ -1,12 +1,7 @@
-# pandoc --chapters -o fp.pdf --latex-engine xelatex \
-#   -f markdown+pipe_tables \
-#   -V papersize:oneside -V links-as-notes \
-#   Functional-programming.md \
-#   Functionals.md
-
-pandoc --chapters -o book/adv-r.pdf --latex-engine xelatex \
+pandoc -o book/adv-r.pdf --latex-engine xelatex \
   --variable=papersize:oneside --variable=links-as-notes \
-  --toc --metadata=monofont:Inconsolata \
+  --toc --chapters --number-sections \
+  --metadata=monofont:Inconsolata \
   book/title.md \
   book/chapters/Introduction.md \
   book/part-foundations.md \
