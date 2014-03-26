02467f878cc0f0fcb0ff0875e6d6f698133bb2a8
hadley <h.wickham@gmail.com>
2014-03-26 10:58:00 -0500

    Profiling up to vectorisation

--- cpp/diff.cpp
+++ cpp/diff.cpp
@@ -2,7 +2,7 @@
 using namespace Rcpp;
 
 // [[Rcpp::export]]
-NumericVector diff2(const NumericVector x, const int lag) {
+NumericVector diff_cpp(const NumericVector x, const int lag = 1) {
   int n = x.length();
   NumericVector y(n - lag);
   for (int i = lag; i < n; i++) {
@@ -10,3 +10,25 @@ NumericVector diff2(const NumericVector x, const int lag) {
   }
   return y;
 }
+
+/*** R
+
+diff_r <- function (x) {
+  xlen <- length(x)
+  if (xlen <= 1) return(x[0L])
+
+  x[-1] - x[-xlen]
+}
+
+
+library(microbenchmark)
+
+x <- runif(1e4)
+stopifnot(all.equal(diff(x), diff2(x)))
+microbenchmark(
+  diff(x),
+  diff_r(x),
+  diff_cpp(x)
+)
+
+*/
