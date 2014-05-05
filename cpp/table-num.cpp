8913d296a7be19a6b5bd8a7c8d040f3dcf09e06e
hadley <h.wickham@gmail.com>
2014-05-05 08:56:25 -0500

    Polishing Rcpp

--- cpp/table-num.cpp
+++ cpp/table-num.cpp
@@ -1,21 +1,18 @@
 #include <Rcpp.h>
-#include <boost/unordered_map.hpp>
 using namespace Rcpp;
 
 // [[Rcpp::export]]
-std::map<double, int> table1(NumericVector x) {
-    
-  // perform the count
-  std::tr1::unordered_map<double, int> counts;
+std::map<double, int> tableC(NumericVector x) {
+  std::map<double, int> counts;
+
   int n = x.size();
   for (int i = 0; i < n; i++) {
     counts[x[i]]++;
   }
-  
-  // sort the results
-  std::map<double, int> out(counts.begin(), counts.end());
-  return out;
+
+  return counts;
 }
+
 
 /*** R
 library(microbenchmark)
@@ -23,7 +20,7 @@ library(microbenchmark)
 x <- sample(1e3, 1e4, rep = T) + 0.5
 microbenchmark(
   table(x),
-  table1(x)
+  tableC(x)
 )
 
 */
