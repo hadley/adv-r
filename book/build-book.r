59cb9121a6b8e46a4de28200463288291f747863
hadley <h.wickham@gmail.com>
2014-05-06 17:29:57 -0500

    Tell book code what target is

--- book/build-book.r
+++ book/build-book.r
@@ -2,10 +2,12 @@ source("_plugins/rmd2html.r")
 
 chapters <- dir(".", pattern = "\\.rmd$")
 
+knitr::opts_knit$set('rmarkdown.pandoc.to' = "latex")
 for (chapter in chapters) {
   message("Processing ", chapter)
   out_path <- paste0("book/chapters/", gsub(".rmd", ".md", chapter))
   out <- rmd2md(chapter, out_path, out = "mdtex")
 }
+knitr::opts_knit$set('rmarkdown.pandoc.to' = NULL)
 
 system("book/build.sh")
