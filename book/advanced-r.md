3d8942915272687e43a598b211011b375f0c4a05
hadley <h.wickham@gmail.com>
2014-05-20 09:22:48 -0500

    Moving towards better book building

---
+++ book/advanced-r.md
@@ -0,0 +1,32 @@
+---
+author: Hadley Wickham
+title: Advanced R
+---
+
+\include{Introduction}
+
+\part{Foundations}
+\include{Data-structures}
+\include{Subsetting}
+\include{Vocabulary}
+\include{Functions}
+\include{OO-essentials}
+\include{Environments}
+\include{Exceptions-debugging}
+
+\part{Functional programming}
+\include{Functional-programming}
+\include{Functionals}
+\include{Function-operators}
+
+\part{Computing on the language}
+\include{Computing-on-the-language}
+\include{Expressions}
+\include{dsl}
+
+\part{Performance}
+\include{Performance}
+\include{Profiling}
+\include{Memory}
+\include{Rcpp}
+\include{C-interface.md}
--- book/build-book.r
+++ book/build-book.r
@@ -1,13 +1,42 @@
-source("_plugins/rmd2html.r")
+library(bookdown)
+library(rmarkdown)
+library(tools)
+
+# Render chapters into tex  ----------------------------------------------------
+needs_update <- function(src, dest) {
+  if (!file.exists(dest)) return(TRUE)
+  mtime <- file.info(src, dest)$mtime
+  mtime[2] < mtime[1]
+}
+
+render_chapter <- function(src) {
+  dest <- file.path("book/tex/", gsub("\\.rmd", "\\.tex", src))
+  if (!needs_update(src, dest)) return()
+
+  message("Rendering ", src)
+  env <- new.env(parent = globalenv())
+
+  render(src, tex_chapter(), output_dir = "book/tex", quiet = TRUE)
+}
 
 chapters <- dir(".", pattern = "\\.rmd$")
+lapply(chapters, render_chapter)
 
-knitr::opts_knit$set('rmarkdown.pandoc.to' = "latex")
-for (chapter in chapters) {
-  message("Processing ", chapter)
-  out_path <- paste0("book/chapters/", gsub(".rmd", ".md", chapter))
-  out <- rmd2md(chapter, out_path, out = "mdtex")
-}
-knitr::opts_knit$set('rmarkdown.pandoc.to' = NULL)
 
-system("book/build.sh")
+# Copy across additional files -------------------------------------------------
+pandoc_convert(wd = "book",
+  "advanced-r.md", output = "tex/advanced-r.tex",
+  options = c(
+    "--template", system.file("book-template.tex", package = "bookdown")
+  )
+)
+file.copy("diagrams/", "book/tex/", recursive = TRUE)
+file.copy("screenshots/", "book/tex/", recursive = TRUE)
+
+pandoc_convert(wd = "book/tex", "advanced-r.tex", output = "advanced-r.pdf",
+  options = c("--latex-engine=xelatex"))
+
+# Build entire latex file
+old <- setwd("book/tex")
+texi2pdf("advanced-r.tex", quiet = FALSE, clean = FALSE)
+# setwd(old)
