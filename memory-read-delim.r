805a8304229c0d06c4e0b730c1aa4b23acc6c389
hadley <h.wickham@gmail.com>
2014-07-24 10:14:21 -0500

    Proof reading chapter 18

--- memory-read-delim.r
+++ memory-read-delim.r
@@ -1,12 +1,13 @@
 # ---- read_delim
 read_delim <- function(file, header = TRUE, sep = ",") {
   # Determine number of fields by reading first line
-  first <- scan(file, what = character(1), nlines = 1, sep = sep, quiet = TRUE)
+  first <- scan(file, what = character(1), nlines = 1,
+    sep = sep, quiet = TRUE)
   p <- length(first)
 
   # Load all fields as character vectors
-  all <- scan(file, what = as.list(rep("character", p)), sep = sep,
-    skip = if (header) 1 else 0, quiet = TRUE)
+  all <- scan(file, what = as.list(rep("character", p)),
+    sep = sep, skip = if (header) 1 else 0, quiet = TRUE)
 
   # Convert from strings to appropriate types (never to factors)
   all[] <- lapply(all, type.convert, as.is = TRUE)
