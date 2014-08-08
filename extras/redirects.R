20d5af123700b9c826d7a7ef8d94ab71246fe4f4
hadley <h.wickham@gmail.com>
2014-08-08 09:12:05 -0400

    Remove r-packages content

---
+++ extras/redirects.R
@@ -0,0 +1,35 @@
+library(whisker)
+
+redirects <- c(
+  "Philosophy.html" = "intro.html",
+  "Package-basics.html" = "description.html",
+  "Package-development-cycle.html" = "r.html",
+  "Package-quick-reference.html" = "r.html",
+  "Documenting-packages.html" = "vignettes.html",
+  "Documenting-functions.html" = "man.html",
+  "Namespaces.html" = "namespace.html",
+  "Style.html" = "style.html",
+  "Testing.html" = "test.html",
+  "Git.html" = "git.html",
+  "Release.html" = "release.html"
+)
+
+data <- list(rule = lapply(seq_along(redirects), function(i) list(old = names(redirects)[i], new = redirects[[i]])))
+
+template <- "
+<RoutingRules>
+{{#rule}}
+  <RoutingRule>
+    <Condition>
+      <KeyPrefixEquals>{{{old}}}</KeyPrefixEquals>
+    </Condition>
+    <Redirect>
+      <HostName>r-pkgs.had.co.nz</HostName>
+      <ReplaceKeyWith>{{{new}}}</ReplaceKeyWith>
+    </Redirect>
+  </RoutingRule>
+{{/rule}}
+</RoutingRules>
+"
+
+cat(whisker.render(template, data))
