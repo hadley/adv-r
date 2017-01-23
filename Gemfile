b1f321cc490c8afc416e47875eca89643d800182
hadley <h.wickham@gmail.com>
2017-01-23 10:03:31 -0600

    Port config across from R packages

---
+++ Gemfile
@@ -0,0 +1,3 @@
+source 'https://rubygems.org'
+gem 'mime-types', '~>2.99'
+gem 'jekyll', '~>2.5'
--- _plugins/knit.r
+++ _plugins/knit.r
@@ -1,4 +1,4 @@
-#!/usr/bin/Rscript
+#!/usr/bin/env Rscript
 library(rmarkdown)
 library(oldbookdown)
 library(methods)
