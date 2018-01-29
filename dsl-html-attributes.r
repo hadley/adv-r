f1b956ca1e30c4393938799ec3a6ceb23fa61add
hadley <h.wickham@gmail.com>
2018-01-29 09:01:46 -0600

    Use rlang in DSLs

--- dsl-html-attributes.r
+++ dsl-html-attributes.r
@@ -1,12 +1,12 @@
 html_attributes <- function(list) {
   if (length(list) == 0) return("")
 
-  attr <- Map(html_attribute, names(list), list)
+  attr <- map2_chr(names(list), list, html_attribute)
   paste0(" ", unlist(attr), collapse = "")
 }
 html_attribute <- function(name, value = NULL) {
   if (length(value) == 0) return(name) # for attributes with no value
-  if (length(value) != 1) stop("value must be NULL or of length 1")
+  if (length(value) != 1) stop("`value` must be NULL or length 1")
 
   if (is.logical(value)) {
     # Convert T and F to true and false
@@ -14,7 +14,7 @@ html_attribute <- function(name, value = NULL) {
   } else {
     value <- escape_attr(value)
   }
-  paste0(name, " = '", value, "'")
+  paste0(name, "='", value, "'")
 }
 escape_attr <- function(x) {
   x <- escape.character(x)
