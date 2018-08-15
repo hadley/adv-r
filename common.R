aba866a2bc52d3718f0a073b2e95bc7b05320921
hadley <h.wickham@gmail.com>
2018-08-15 10:09:25 -0500

    Eliminate chunk colouriser
    Will try again in the future with fansi

--- common.R
+++ common.R
@@ -16,24 +16,6 @@ knitr::opts_chunk$set(
   fig.show = "hold"
 )
 
-# options("crayon.enabled" = TRUE, "crayon.colors" = 256L)
-# crayon::num_colors(TRUE)
-#
-# colourise_chunk <- function(x, options) {
-#   fence = paste(rep("`", 3), collapse = '')
-#
-#   x = gsub(paste0('[\n]{2,}(', fence, '|    )'), '\n\n\\1', x)
-#   x = gsub('[\n]+$', '', x)
-#   x = gsub('^[\n]+', '\n', x)
-#   if (isTRUE(options$collapse)) {
-#     x = gsub(paste0('\n([', fence_char, ']{3,})\n+\\1(', tolower(options$engine), ')?\n'), "\n", x)
-#   }
-#   x <- ansistrings::ansi_to_html(x, fullpage = FALSE)
-#
-#   if (is.null(s <- options$indent)) return(x)
-#   line_prompt(x, prompt = s, continue = s)
-# }
-
 knitr::knit_hooks$set(
   small_mar = function(before, options, envir) {
     if (before) {
