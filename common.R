5fffe644a190d1e692adecd4bdbc178d995c1eb1
hadley <h.wickham@gmail.com>
2018-08-15 10:07:56 -0500

    Explain retina setting

--- common.R
+++ common.R
@@ -7,7 +7,7 @@ knitr::opts_chunk$set(
   comment = "#>",
   collapse = TRUE,
   cache = TRUE,
-  fig.retina = 1,
+  fig.retina = 1, # figures are either vectors or 300 dpi diagrams
   dpi = 300,
   out.width = "70%",
   fig.align = 'center',
@@ -67,4 +67,3 @@ end_sidebar <- function() {
     knitr::asis_output("</div>\n")
   }
 }
-
