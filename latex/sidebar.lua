9d04a7926c628067b2abd5d312aa1e6cad36b3d0
hadley <h.wickham@gmail.com>
2018-08-28 16:32:32 -0500

    Custom filters for base and todo

--- latex/sidebar.lua
+++ latex/sidebar.lua
@@ -7,4 +7,22 @@ function Div(el)
       pandoc.RawBlock("latex", "\\end{kframe}")
     }
   end
+
+  if el.classes:includes("base") then
+    return {
+      pandoc.RawBlock("latex", "\\begin{kframe} \\textbf{In Base R}"),
+      el,
+      pandoc.RawBlock("latex", "\\end{kframe}")
+    }
+  end
+end
+
+function Span(el)
+  if el.classes:includes("todo") then
+    return {
+      pandoc.RawInline("latex", "\\textcolor{red}{\\textbf{TODO: }"),
+      el,
+      pandoc.RawInline("latex", "}")
+    }
+  end
 end
