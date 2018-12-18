ea63f1e48fb523c013fb3df1860b7e0c227e1512
Hadley Wickham <h.wickham@gmail.com>
2018-12-18 17:00:32 -0600

    Finishing up perf measurements

--- memory-read-delim.r
+++ memory-read-delim.r
@@ -1,16 +1,17 @@
 read_delim <- function(file, header = TRUE, sep = ",") {
   # Determine number of fields by reading first line
-  first <- scan(file, what = character(1), nlines = 1, sep = sep, quiet = TRUE)
+  first <- scan(
+    file, what = character(1), nlines = 1,
+    sep = sep, quiet = TRUE
+  )
   p <- length(first)
 
   # Load all fields as character vectors
   all <- scan(
-    file,
-    what = as.list(character(p)),
-    sep = sep,
-    skip = if (header) 1 else 0,
-    quiet = TRUE
+    file, what = as.list(character(p)), sep = sep,
+    skip = if (header) 1 else 0, quiet = TRUE
   )
+  gc()
 
   # Convert from strings to appropriate types (never to factors)
   all[] <- lapply(all, type.convert, as.is = TRUE)
